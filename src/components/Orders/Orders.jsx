2fb0eba88d3bae152513cb3b299c0d3d75cae9b2
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-05 05:53:55 +0600

    product add and remove function added

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import ReviewItems from '../ReviewItems/ReviewItems';
 import './Orders.css';
 
 const Orders = () => {
-    const cart = useLoaderData();
-    // console.log(cart);
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart);
+
+    const handleRemoveFromCart = (id) => {
+        const remaining = cart.filter(product => product.id !==id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
+ 
     return (
         <div className='shop-container'>
             <div className='review-container'>
-            <h1>The Orders Page {cart.length}</h1> 
+            <h1>The Orders Page {savedCart.length}</h1> 
             {
-                cart.map(product => <ReviewItems 
+               cart.map(product => <ReviewItems 
                 key={product.id}
                 product={product}
-                
+                handleRemoveFromCart={handleRemoveFromCart}
                 ></ReviewItems>)
             }
            
--- src/components/ReviewItems/ReviewItems.jsx
+++ src/components/ReviewItems/ReviewItems.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './ReviewItems.css';
-const ReviewItems = ({product}) => {
+const ReviewItems = ({product, handleRemoveFromCart}) => {
     const {id, img, name, seller, price, quantity, shipping} = product;
-console.log(product);
+
 
     return (
         <div className='review-itms'>
             <img src={img} alt="" />
             <div className='review-details'>
                 <p>{name}</p>
-                <p>price: <span className='orange-text'></span></p>
+                <p>price: 
+                    <span className='orange-text'> {price}</span> 
+                    <span> *</span>
+                    <span className='orange-text'> {quantity}</span>
+                    </p>
+                <p > shipping:<span className='orange-text'> {shipping}                    
+                    </span> 
+                    </p>
+            </div>
+            <div>
+            <span onClick={()=>handleRemoveFromCart(id)} className='btn-delete'>delete</span>
             </div>
         </div>
     );
