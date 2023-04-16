1080508ebad37c2c95dbdbb749f902cd4849a0ec
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-17 04:08:04 +0600

    Firebase installed and sign in method activeted

---
+++ src/components/SignUp/SingUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingUp = () => {
+    return (
+        <div>
+            <p>Sign Up page</p>
+        </div>
+    );
+};
+
+export default SingUp;
---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAliHlLUwwnCEfcuJ1Q-R5MRa0KlEGvr4Y",
+  authDomain: "ema-john-aws.firebaseapp.com",
+  projectId: "ema-john-aws",
+  storageBucket: "ema-john-aws.appspot.com",
+  messagingSenderId: "930993544106",
+  appId: "1:930993544106:web:6c6716412996f391d829d3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
